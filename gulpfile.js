84da74dd18c2bab26df8dd29c81437decf0b4156
Feroz Abdul Rehman <feroz.rehman@salesforce.com>
2019-04-02 16:39:38 +0100

    fixed -  Found fs.readdirSync with non literal argument at index 0 eslint(security/detect-non-literal-fs-filename)

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const gulp = require('gulp');
 
-// prettier-ignore
+//explicitly require each file in gulp folder
+require('./gulp/server');
+
+/* Causes error: Found fs.readdirSync with non literal argument at index 0  eslint(security/detect-non-literal-fs-filename)
 fs
   .readdirSync(__dirname + '/gulp')
   .forEach(task => {
     require('./gulp/' + task);
 });
-
+*/
 gulp.task('dev', gulp.series('dev:server'));
