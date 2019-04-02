5f531a3126bfffcd994e7cc3e83a65a79ac55caf
Feroz Abdul Rehman <feroz.rehman@salesforce.com>
2019-04-02 15:41:58 +0100

    Removed // prettier-ignore

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 
-// prettier-ignore
 router
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({ extended: true }))
