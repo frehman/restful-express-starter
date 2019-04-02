6ff820419337042eab92930a32ac36c1248cefd9
Feroz Abdul Rehman <feroz.rehman@salesforce.com>
2019-04-02 15:41:35 +0100

    Added HATEOAS

--- src/controllers/api/user.js
+++ src/controllers/api/user.js
@@ -2,12 +2,29 @@ const router = require('express').Router();
 const User = require('../../models/User');
 const log = require('../../logger');
 
-// prettier-ignore
 router
   .get('/', (req, res) => {
-    User.findAll().then(users => {
-      log.info({ itemCount:Object.keys(users).length }, `Count of number of items returned`);
-      res.status(200).json(users);
+    User.findAll({
+      attributes: ['id', 'first_name', 'last_name']
+    }).then(users => {
+      log.info(
+        { itemCount: Object.keys(users).length },
+        `Count of number of items returned`
+      );
+      const returnedUsers = users.map(user => {
+        const returnedUser = user.toJSON();
+        returnedUser.links = {};
+        returnedUser.links.details = `http://${req.headers.host}/api/user/${
+          user.id
+        }`;
+        return returnedUser;
+      });
+      res.status(200).json(returnedUsers);
+    });
+  })
+  .get('/:userid', (req, res) => {
+    User.findByPk(req.params.userid).then(user => {
+      res.status(200).json(user);
     });
   })
   .post('/', (req, res) => {
