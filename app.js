7bcc77cac530c1776b331e18afcb0149806a7196
Feroz Abdul Rehman <feroz.rehman@salesforce.com>
2019-06-26 11:53:26 +0100

    node clustering with throng

--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const express = require('express');
 const helmet = require('helmet');
 const log = require('./src/logger');
+const throng = require('throng');
 
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
 /**
  * The `heroku local` command usually sets the port to 5000
  */
 const port = process.env.PORT || 3000;
-const app = express();
+throng(
+  {
+    workers: WORKERS,
+    lifetime: Infinity
+  },
+  start
+);
 
-/**
- * Setup the express server
- * ./src/controllers/index sets up the routes
- */
-app
-  .use(require('./src/controllers'))
-  .use(helmet())
-  .get('/', (req, res) => {
-    res.send('Hello World API.');
-  })
-  .listen(port, () => {
-    log.info({ port: port }, 'Express port number');
-  });
+function start() {
+  const app = express();
 
-module.exports = app;
+  /**
+   * Setup the express server
+   * ./src/controllers/index sets up the routes
+   */
+  app
+    .use(require('./src/controllers'))
+    .use(helmet())
+    .get('/', (req, res) => {
+      res.send('Hello World API.');
+    })
+    .listen(port, () => {
+      log.info({ port: port }, 'Express port number');
+    });
+
+  module.exports = app;
+}
